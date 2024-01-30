b02078ce1b376e3fbe4b348343948499777c0c2d
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-01-30 16:24:32 +0100

    Add GlossaryResource TCA

---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_resource.php
@@ -0,0 +1,63 @@
+<?php
+defined('TYPO3') or die();
+
+# This file is part of the extension CHF Gloss for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+/**
+ * GlossaryResource and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add select item 'glossaryResource'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem('tx_chfbase_domain_model_resource', 'type',
+    [
+        'label' => 'LLL:EXT:chf_gloss/Resources/Private/Language/locallang.xlf:object.glossaryResource.type.glossaryResource',
+        'value' => 'glossaryResource',
+    ]
+);
+
+// Add column 'allGlossaryEntries'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_resource',
+    [
+        'allGlossaryEntries' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_gloss/Resources/Private/Language/locallang.xlf:object.glossaryResource.allGlossaryEntries',
+            'description' => 'LLL:EXT:chf_gloss/Resources/Private/Language/locallang.xlf:object.glossaryResource.allGlossaryEntries.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfgloss_domain_model_glossary_entry',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+    ]
+);
+
+// Add type 'glossaryResource' and its 'showitem' list
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+   'tx_chfbase_domain_model_resource',
+   'hiddenUuid,typeUri,titleLangCode,description,sameAs,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.editorial,authorshipRelation,licenceRelation,publicationDateRevisionNumberRevisionDate,editorialNote,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.content,allAgents,allFileCollections,allLocations,allPeriods,allRelations,allTags,allGlossaryEntries,
+   --div--;LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:object.generic.import,importOrigin,importState,'
+   'glossaryResource'
+);

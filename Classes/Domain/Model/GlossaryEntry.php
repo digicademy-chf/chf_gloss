1386fb3df454da9f3d7700239ab09ce3500511ab
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-14 00:03:00 +0200

    Add GlossaryEntry

---
+++ Classes/Domain/Model/GlossaryEntry.php
@@ -0,0 +1,306 @@
+<?php
+declare(strict_types=1);
+
+# This file is part of the extension CHF Gloss for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+namespace Digicademy\CHFGloss\Domain\Model;
+
+use TYPO3\CMS\Extbase\Annotation\ORM\Lazy;
+use TYPO3\CMS\Extbase\Annotation\Validate;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+use Digicademy\CHFBase\Domain\Validator\StringOptionsValidator;
+
+defined('TYPO3') or die();
+
+/**
+ * Model for GlossaryEntry
+ */
+class GlossaryEntry extends AbstractEntity
+{
+    /**
+     * Whether the record should be visible or not
+     * 
+     * @var bool
+     */
+    #[Validate([
+        'validator' => 'Boolean',
+    ])]
+    protected bool $hidden = true;
+
+    /**
+     * Resource that this database record is part of
+     * 
+     * @var ?ObjectStorage<object>
+     */
+    #[Lazy()]
+    protected ?ObjectStorage $parentResource = null;
+
+    /**
+     * Unique identifier of this database record
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'RegularExpression',
+        'options' => [
+            'regularExpression' => '^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$',
+            'errorMessage' => 'LLL:EXT:chf_base/Resources/Private/Language/locallang.xlf:validator.regularExpression.noUuid',
+        ],
+    ])]
+    protected string $uuid = '';
+
+    /**
+     * Specific type of glossary entry
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => StringOptionsValidator::class,
+        'options'   => [
+            'allowed' => [
+                'regular',
+                'abbreviation',
+            ],
+        ],
+    ])]
+    protected string $type = 'regular';
+
+    /**
+     * Term to be defined
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'StringLength',
+        'options' => [
+            'maximum' => 255,
+        ],
+    ])]
+    protected string $term = '';
+
+    /**
+     * Comma-separated list of further strings to annotate, e.g., inflected forms
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'StringLength',
+        'options' => [
+            'maximum' => 255,
+        ],
+    ])]
+    protected string $additionalStrings = '';
+
+    /**
+     * Brief explanation of the term in question
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'String',
+    ])]
+    protected string $description = '';
+
+    /**
+     * Construct object
+     *
+     * @param object $parentResource
+     * @param string $uuid
+     * @param string $type
+     * @param string $term
+     * @param string $description
+     * @return GlossaryEntry
+     */
+    public function __construct(object $parentResource, string $uuid, string $type, string $term, string $description)
+    {
+        $this->initializeObject();
+
+        $this->addParentResource($parentResource);
+        $this->setUuid($uuid);
+        $this->setType($type);
+        $this->setTerm($term);
+        $this->setDescription($description);
+    }
+
+    /**
+     * Initialize object
+     */
+    public function initializeObject(): void
+    {
+        $this->parentResource ??= new ObjectStorage();
+    }
+
+    /**
+     * Get hidden
+     *
+     * @return bool
+     */
+    public function getHidden(): bool
+    {
+        return $this->hidden;
+    }
+
+    /**
+     * Set hidden
+     *
+     * @param bool $hidden
+     */
+    public function setHidden(bool $hidden): void
+    {
+        $this->hidden = $hidden;
+    }
+
+    /**
+     * Get parent resource
+     *
+     * @return ObjectStorage<object>
+     */
+    public function getParentResource(): ?ObjectStorage
+    {
+        return $this->parentResource;
+    }
+
+    /**
+     * Set parent resource
+     *
+     * @param ObjectStorage<object> $parentResource
+     */
+    public function setParentResource(ObjectStorage $parentResource): void
+    {
+        $this->parentResource = $parentResource;
+    }
+
+    /**
+     * Add parent resource
+     *
+     * @param object $parentResource
+     */
+    public function addParentResource(object $parentResource): void
+    {
+        $this->parentResource?->attach($parentResource);
+    }
+
+    /**
+     * Remove parent resource
+     *
+     * @param object $parentResource
+     */
+    public function removeParentResource(object $parentResource): void
+    {
+        $this->parentResource?->detach($parentResource);
+    }
+
+    /**
+     * Remove all parent resources
+     */
+    public function removeAllParentResource(): void
+    {
+        $parentResource = clone $this->parentResource;
+        $this->parentResource->removeAll($parentResource);
+    }
+
+    /**
+     * Get UUID
+     *
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Set UUID
+     *
+     * @param string $uuid
+     */
+    public function setUuid(string $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get term
+     *
+     * @return string
+     */
+    public function getTerm(): string
+    {
+        return $this->term;
+    }
+
+    /**
+     * Set term
+     *
+     * @param string $term
+     */
+    public function setTerm(string $term): void
+    {
+        $this->term = $term;
+    }
+
+    /**
+     * Get additional strings
+     *
+     * @return string
+     */
+    public function getAdditionalStrings(): string
+    {
+        return $this->additionalStrings;
+    }
+
+    /**
+     * Set additional strings
+     *
+     * @param string $additionalStrings
+     */
+    public function setAdditionalStrings(string $additionalStrings): void
+    {
+        $this->additionalStrings = $additionalStrings;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}

71128eee20a2be463056aae581fbd7e137294963
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-25 10:16:41 +0200

    Move MM table config to the right extensions

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,10 @@
 # Remove after TYPO3 13.2 when all fields are being auto-created
 
+CREATE TABLE tx_chfbase_domain_model_resource_resource_glossary_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL,
+	tablenames varchar(63) DEFAULT '' NOT NULL
+);
+
 CREATE TABLE tx_chfgloss_domain_model_glossary_entry (
     term varchar(255) DEFAULT '' NOT NULL,
     additionalStrings varchar(255) DEFAULT '' NOT NULL

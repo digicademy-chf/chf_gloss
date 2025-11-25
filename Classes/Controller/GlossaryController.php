e94abf1e4c49c6f375891565cb80e1d6bbbfb43d
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2025-11-25 17:11:18 +0100

    Park ongoing frontend work for glossaries

--- Classes/Controller/GlossaryController.php
+++ Classes/Controller/GlossaryController.php
@@ -38,8 +38,32 @@ class GlossaryController extends ActionController
         // Get resource
         $resourceIdentifier = $this->settings['resource'];
         $resource = $this->abstractResourceRepository->findByIdentifier($resourceIdentifier);
-        $glossaryEntries = $resource->getItems()->toArray();
-        $this->view->assign('glossaryEntries', $glossaryEntries);
+
+        // DEBUG
+        #$this->view->assign('resource', $resource);
+        //$glossaryEntries = $resource->getItems()->toArray();
+        //$this->view->assign('glossaryEntries', $glossaryEntries);
+        // CHECK HIDDEN STATE, TURN INTO PARTIAL AND ALSO USE IN PLUGIN TEMPLATE
+        // MERGE authorshipRelation OF RESOURCE WITH RECORD
+
+        // Compile relevant data
+        if(!$resource->getHidden() && $resource->getType() == 'glossaryResource') {
+            $this->view->assignMultiple([
+                'uid' => $resource->getUid(),
+                'title' => $resource->getTitle(),
+                'langCode' => $resource->getLangCode(),
+                'description' => $resource->getDescription(),
+                'items' => $resource->getItems(), # TODO, some issue, unprotect, group by entry types
+                'iri' => $resource->getIri(),
+                'uuid' => $resource->getUuid(),
+                'sameAs' => $resource->getSameAs(), # TODO, unprotect
+                'publicationDate' => $resource->getPublicationDate(),
+                'revisionDate' => $resource->getRevisionDate(),
+                'revisionNumber' => $resource->getRevisionNumber(),
+                #'authorshipRelation' => $resource->getAuthorshipRelation(), # TODO, SQL-Fehler, unprotect
+                #'licenceRelation' => $resource->getLicenceRelation(), # TODO, SQL-Fehler, unprotect
+            ]);
+        }
 
         // Create response
         return $this->htmlResponse();
